3ffb3f7482df27f893c5aeb9a1a818f2d77858cf
dandolodavid <dandolodavid@gmail.com>
2021-06-21 13:53:08 +0200

    first commit

---
+++ iET/iET.py
@@ -0,0 +1,130 @@
+import pandas as pd
+import numpy as np
+from tqdm import tqdm
+import plotly.express as px
+import plotly.figure_factory as ff
+import plotly.graph_objects as go
+from iET.ier_functions import *
+
+class iET():
+
+    def __init__(self, touchpoint_colname, target_colname, client_id_colname, session_colname = None, use_top_k = None):
+        self._touchpoint_colname = touchpoint_colname
+        self._target_colname = target_colname
+        self._client_id_colname = client_id_colname
+        self._session_colname = session_colname
+        self._top_k = use_top_k
+
+    def estimate_proba(self, dataframe ):
+        '''
+        Prepare the dataframe and estimate all the required probabilities
+        '''
+        data = dataframe.copy()
+
+        #if session is used we create a new key that is the joined of client id and session id
+        if self._session_colname:
+            dataframe['client_session_id'] = dataframe[self._client_id_colname].astype(str) +' : ' + dataframe[self._session_colname].astype(str)
+            self._client_session_colname = 'client_session_id'
+        else:
+            self._client_session_colname = self._client_id_colname
+
+        if self._top_k:
+            top_touchpoint = count_source_medium(dataframe, self._touchpoint_colname, self)
+            top_k = count_source_medium.sort_values('cdp_id').tail(self._top_k).index
+            data = data.loc[ data[self._touchpoint_colname].apply(lambda x: x in top_touchpoint) ]
+            
+        self._count_df = count_source_medium(data, self._touchpoint_colname, self._client_session_colname)
+
+        #compute probability of reach the target
+        target_df = target_reached_probability( data, self._client_session_colname, self._touchpoint_colname, self._target_colname)
+
+        #compute quit probability
+        quit_df = quit_probability( data, self._client_session_colname, self._touchpoint_colname, self._target_colname )
+
+        #estimate transaction matrix
+        self._transaction_df, self._count_transaction_df = estimate_transaction_matrix( data, self._client_session_colname, self._client_id_colname, self._touchpoint_colname, self._target_colname )
+
+        #compute move proabability
+        move_df = move_probabilities(target_df, quit_df)
+
+        #compute expected target
+        xT_df = expected_target(data, self._target_colname, self._touchpoint_colname)
+
+        #aggregate all the info and compute the attribution values
+        self._final_df = merge_elements(target_df, quit_df, move_df, xT_df)
+    
+    def estimate_attribution(self, max_iter = 20, epsilon = 0.1):
+        '''
+        '''
+        self._iter_solution, self._diff_df = calculate_attribution_values( self._final_df, self._transaction_df, max_iter , epsilon )
+        attribution_df = pd.DataFrame( self._iter_solution.iloc[:,-1] ) 
+        attribution_df.columns = ['attribution_values']
+        self._attribution_df = attribution_df.sort_values(by = 'attribution_values', ascending = False)
+        
+        return self._attribution_df.copy()
+    
+    def plot(self):
+        '''
+        '''
+        plot_df = self._attribution_df.merge(self._count_df, left_index=True, right_index=True)
+        size = plot_df[self._client_session_colname].values
+
+        fig = go.Figure(data=[go.Bar(
+            x = plot_df.index.tolist(),
+            y = plot_df.attribution_values.values,
+            width = size/max(size) # customize width here
+        )])
+        
+        fig.update_layout(title={'text': 'Attribution values ( bar width is proportional to the number of users and sessions for that touchpoint )' })
+
+        return fig
+    
+    def plot_proba(self):
+        '''
+        '''
+        plot_df = self._final_df[['target_prob','quit_prob','move_prob']].stack().reset_index()
+        plot_df.columns = [self._touchpoint_colname, 'hue', 'value']
+        fig = px.bar(plot_df, x = self._touchpoint_colname, y = 'value', color = 'hue', title= 'Buy Quit and Move probability by channels')
+        return fig
+    
+    def plot_transaction(self):
+        '''
+        '''
+        z = self._transaction_df.values
+
+        x = self._transaction_df.columns.tolist()
+        y = self._transaction_df.index.tolist()
+
+        z_text = self._transaction_df.astype(str).values
+        
+        fig = ff.create_annotated_heatmap(z, x=x, y=y, annotation_text=z_text, colorscale = 'bluered')
+        return fig
+    
+    def plot_ExT(self):
+        '''
+        '''
+        plot_df = self._final_df.merge(self._count_df, left_index=True, right_index=True)
+        size = plot_df[self._client_session_colname].values
+        
+        fig = go.Figure(data=[go.Bar(
+            x = plot_df.index.tolist(),
+            y = plot_df.xT.values,
+            width = size/max(size) # customize width here
+        )])
+        
+        fig.update_layout(title={'text': 'Expected target values ( bar width is proportional to the number of users and sessions for that touchpoint )' })
+
+        return fig
+    
+    def proba_df(self):
+        return self._final_df
+    
+
+
+    
+
+
+
+        
+        
+
---
+++ iET/iet_functions.py
@@ -0,0 +1,208 @@
+import pandas as pd
+import numpy as np
+from tqdm import tqdm
+
+def count_source_medium( data, touchpoint, client_session_id ):
+    return data.groupby(touchpoint).count()[[client_session_id]]   
+
+def target_reached_probability( data, client_session_id, touchpoint, target):
+    '''
+    Compute the probability to be last touchpoint before the target
+
+    1) Find unique client_session + touchpoint combinations
+    2) Groups by touchpoint and count the session with target reached
+    3) Compute the target probability
+    
+    Parameters:
+    -----------
+    data: dataframe
+    client_session_id : unique ids for client session colname
+    touchpoint: colname with the touchpoint label
+    target: target colname
+    
+    Returns:
+    ---------
+    touchpoint_target_prob : dataframe with target probabilities
+    '''
+
+    #all occurences of all the combinations client/session + touchpoint 
+    combinations = data.drop_duplicates( subset =  [ client_session_id, touchpoint ] )[[ client_session_id, touchpoint ]]
+    target_combinations = data.loc[ data[target] != 0 ].drop_duplicates( subset =  [ client_session_id, touchpoint ] )[[ client_session_id, touchpoint ]]
+
+    base = combinations.groupby(touchpoint).count()[[client_session_id]]
+    has_target = target_combinations.groupby(touchpoint).count()[[client_session_id]]
+
+    touchpoint_target_prob = base.merge(has_target, left_index=True, right_index=True, how='left').fillna(0)
+    touchpoint_target_prob.columns = ['base','has_target']
+    touchpoint_target_prob['prob'] = touchpoint_target_prob['has_target'] / touchpoint_target_prob['base']
+
+    return touchpoint_target_prob
+
+def quit_probability( data, client_session_id, touchpoint, target):
+    '''
+    Compute the probability to be last touchpoint before quit 
+
+    1) Find unique client_session + touchpoint combinations
+    2) Groups by touchpoint and count the session with target reached
+    3) Compute the target probability
+    
+    Parameters:
+    -----------
+    data: dataframe
+    client_session_id : unique ids for client session colname
+    touchpoint: colname with the touchpoint label
+    target: target colname
+    
+    Returns:
+    ---------
+    touchpoint_quit_prob : dataframe with quit probabilities
+    '''
+
+    #all occurences of all the combinations client/session + touchpoint for all the data and only for zero data
+    combinations = data.drop_duplicates( subset =  [ client_session_id, touchpoint ] )[[ client_session_id, touchpoint ]]
+    quit_combinations = data.loc[ data[target] == 0 ].drop_duplicates( subset =  [ client_session_id ], keep = 'last' )[[ client_session_id, touchpoint ]]
+
+    base = combinations.groupby(touchpoint).count()[[client_session_id]]
+    has_quit = quit_combinations.groupby(touchpoint).count()[[client_session_id]]
+
+    touchpoint_quit_prob = base.merge(has_quit, left_index=True, right_index=True, how='left').fillna(0)
+    touchpoint_quit_prob.columns = ['base','has_quit']
+    touchpoint_quit_prob['prob'] = touchpoint_quit_prob['has_quit'] / touchpoint_quit_prob['base']
+
+    return touchpoint_quit_prob
+
+def estimate_transaction_matrix( data, client_id, client_session_id, touchpoint, target ):
+    '''
+    Compute transaction matrix probabilities
+
+    Parameters:
+    -----------
+    data: dataframe
+    client_session_id : unique ids for client session colname
+    touchpoint: colname with the touchpoint label
+    target: target colname
+    
+    Returns:
+    ---------
+    prob_transiction : dataframe with transaction probabilities
+    count_transiction : dataframe with transaction count
+    '''
+
+    #remove consecuitve duplicates of the same session in the same touchpoint
+    shifted = data.shift(1)
+    client_touchpoint_path = data.loc[ np.logical_or( shifted[client_session_id] != data[client_session_id] , shifted[touchpoint] != data[touchpoint] ) ].reset_index(drop=True)
+
+    #detect change in client_id 
+    #TO READ: here we use only the client id not the client session id. In this way when there are session there is the possiblity to cambe back with the same touchpoint
+    # when tere are no session the same user could not move to the same touchpoint 
+
+    client_touchpoint_path['is_client_changed'] = client_touchpoint_path[client_id].shift(1, fill_value = client_touchpoint_path[client_id].head(1))  !=  \
+                                                client_touchpoint_path[client_id]
+
+    change_index = client_touchpoint_path.loc[client_touchpoint_path['is_client_changed']].index
+
+    #add none where the client change, to easily remove that line from the count
+    partial = pd.DataFrame(None, index= change_index - 0.5)
+    partial[touchpoint] = None
+
+    touchpoint_state = client_touchpoint_path[[touchpoint]].copy()
+    touchpoint_state = touchpoint_state.append(partial).sort_index().reset_index(drop=True)
+
+    #create the shift and drop the line with None: they are last status of the user 
+    touchpoint_state['shift'] = touchpoint_state[touchpoint].shift(-1)
+    touchpoint_state['count'] = 1
+    touchpoint_state = touchpoint_state.dropna()
+
+    #groupby and count the combinations touchpoint-shift
+    count_transiction = touchpoint_state.groupby([touchpoint, 'shift']).count().unstack().fillna(0)
+    count_transiction.columns = count_transiction.columns.get_level_values(1)
+    count_transiction.index.name=None
+    count_transiction.columns.name=None
+
+    for tp in data[touchpoint].unique().tolist():
+        if tp not in count_transiction.columns.tolist():
+            count_transiction[tp] = 0
+
+        if tp not in count_transiction.index.values:
+            count_transiction.loc[tp] = np.zeros(count_transiction.shape[1])
+
+    
+    prob_transiction = pd.DataFrame( np.round(count_transiction.values / (count_transiction.sum(axis=1).values[:,None] + 0.000000001) ,4) ,\
+                                            index = count_transiction.index, columns=count_transiction.columns )
+    
+
+    return prob_transiction, count_transiction
+
+def move_probabilities(target_probabilities, quit_probabilitis):
+    '''
+    Find the probability to move to another touchpoint
+
+    Returns:
+    -------
+    dataframe with the move probabilities
+    '''
+    return pd.DataFrame( np.round(1 - target_probabilities.prob - quit_probabilitis.prob, 4 ) )
+
+def expected_target(data, target, touchpoint):
+    '''
+    Compute the expected target value
+    
+    Returns:
+    --------
+    dataframe with the expcted target value conditioned to target > 0
+    '''
+    return data.loc[ data[target] > 0 ].groupby( touchpoint ).mean()[ [target] ]
+
+def merge_elements(target_df, quit_df, move_df, xT_df):
+    '''
+    Create unique df merging the result of target_reached_probability, quit_probability, move_probabilities, expected_target
+
+    Returns:
+    ----------
+    final_df : dataframe
+    '''
+
+    final_df = target_df[['prob']].merge(quit_df[['prob']], left_index=True, right_index=True)
+    final_df = final_df.merge(move_df, left_index=True, right_index=True)
+    final_df = final_df.merge(xT_df, left_index=True, right_index=True)
+    final_df.columns=['target_prob','quit_prob','move_prob','xT']
+
+    return final_df
+
+def calculate_attribution_values( final_df, prob_transiction, max_iter = 20, epsilon = 0.1 ):
+    '''
+    Calculate the attribution values and evaluate the convergences of the algorithm.
+    The cicle end when max iter is reached or the differences between consecutive state sum is less than epsilon
+
+    Parameters:
+    ----------
+    final_df : dataframe form merge_elments function
+    prob_transiction : probabilities transaction matrix from estimate_transaction_matrix
+    max_iter : int, max number of cycle
+    epsilon: float, minimium differences sum to reach to stop the iteractions
+
+    Returns:
+    --------
+    values_df: dataframe with all the attribution values state
+    diff_df: dataframe with the story of differences
+
+    '''
+    tp_list = final_df.index.tolist()
+    values_df = pd.DataFrame( np.zeros(len(tp_list)), index = tp_list, columns = ['v0'])
+    diff_df = pd.DataFrame( np.zeros(len(tp_list)), index = tp_list, columns = ['dv0'])
+    
+    for itr in range(0,max_iter):
+        
+        values_df['v' + str( itr+1 )] = None
+        
+        for tp in tp_list:
+            values_df.loc[tp, 'v' + str( itr+1 )] = final_df.loc[ tp,'target_prob' ]*final_df.loc[ tp,'xT' ] + \
+            final_df.loc[ tp,'move_prob' ] * np.sum( values_df[ 'v'+ str(itr) ] * prob_transiction.loc[tp][tp_list] )
+        
+        diff_df['dv'+str(itr)] = values_df[ 'v' + str(itr+1) ] - values_df[ 'v' + str(itr) ]   
+
+        if diff_df['dv'+str(itr)].sum() < epsilon:
+            print('Convergence reached in ' +str(itr)+ ' passes')
+            break
+
+    return values_df, diff_df
